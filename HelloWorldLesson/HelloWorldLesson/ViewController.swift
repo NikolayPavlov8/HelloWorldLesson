7033c813d1442e4bf6a092117a543398eccaeb70
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-14 19:09:49 +0300

    code1

---
+++ HelloWorldLesson/HelloWorldLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldLesson
+//
+//  Created by Nikolai Pavlov on 14/05/2019.
+//  Copyright © 2019 Nikolai Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
